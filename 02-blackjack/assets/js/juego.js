a077a0cf8a5c1279ac4f06a83a537e0ffad2f559
dhren2019 <51840371+dhren2019@users.noreply.github.com>
2020-03-20 18:57:50 +0100

    Cartas

---
+++ 02-blackjack/assets/js/juego.js
@@ -0,0 +1,30 @@
+/**
+ * 2C = two of clubs
+ * 2D = two fiamonds
+ * 2H = two hearts
+ * 2S = two spades
+ * 
+ */
+
+ let deck = [];
+
+ const tipos = ['C', 'D', 'H', 'S'];
+
+ const crearDeck = () => {
+
+     /*i = 2 porque las cartas comienzan en 2
+     *i <= es el numero mayor de las cartas
+     y se incrementa en 1
+     */
+    for ( let i = 2; i <= 10; i++){
+
+        for( let tipo of tipos) {
+
+            //i vale de 2 hasta 10
+        }
+
+    }
+
+ }
+  
+ crearDeck();
